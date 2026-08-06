c9f81c33b41d8430cc95a21ab72f70ab990c9db2
Harsh Vardhan Dubey <113812998+harsh-dubey10@users.noreply.github.com>
2026-08-06 15:53:38 +0530

    Remove Achievements and ScrollProgress components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import Achievements from './components/Achievements'
 import Contact from './components/Contact'
 import Gallery from './components/Gallery'
 import Hero from './components/Hero'
 import Nav from './components/Nav'
 import Profile from './components/Profile'
-import ScrollProgress from './components/ScrollProgress'
 import Timeline from './components/Timeline'
 import Work from './components/Work'
 
 export default function App() {
   return (
     <div className="relative">
-      <ScrollProgress />
       <Nav />
       <main className="mx-auto max-w-rail px-7">
         <Hero />
         <Profile />
-        <Achievements />
         <Timeline />
         <Work />
         <Gallery />
